285670361942616d8efc23be1a70d72f3988b264
PhilBroderick <philbroderick12@gmail.com>
2022-03-10 19:07:29 +0000

    add basic ip validator

---
+++ src/GeoLocator.Infrastructure/Services/IpValidator.cs
@@ -0,0 +1,8 @@
+﻿using GeoLocator.Core.Interfaces;
+using System.Net;
+
+namespace GeoLocator.Infrastructure.Services;
+public class IpValidator : IIpValidator
+{
+    public bool ValidateIp(string ip) => IPAddress.TryParse(ip, out _);
+}
